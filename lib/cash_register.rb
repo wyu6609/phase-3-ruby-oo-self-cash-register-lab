d05a79b277b87ce83a6df394e5dfc591f5ebdac2
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-03-16 11:24:34 -0400

    commit tree 584d6f1aaa237280beb86296cf3b7e7b6c812d0e

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+require 'pry'
 
+class CashRegister
+  attr_reader :discount, :items
+  attr_writer :total
+
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+    @items = []
+  end
+
+  def total
+    @total
+  end
+
+  def add_item(name, price, quantity = 1)
+    self.total += price * quantity
+    @last_item = price * quantity
+
+    quantity.times { @items.push name }
+  end
+
+  def items
+    @items
+  end
+
+  def apply_discount
+    msg = ''
+
+    if self.discount > 0
+      self.total *= (1 - self.discount / 100.0)
+      msg = "After the discount, the total comes to $#{self.total.to_i}."
+    else
+      msg = 'There is no discount to apply.'
+    end
+
+    msg
+  end
+
+  def void_last_transaction
+    self.total -= @last_item
+  end
+end
